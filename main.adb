70e0b7e0d84e8327870d4296829c5aed9ba8a752
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-15 20:22:33 +0200

    Create main.adb for Double Dabble Algorithm

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Double Dabble Algorithm Project.");
+   Put_Line ("Please run 'make test' to execute the validation suite.");
+end Main;
